aa4229d35cd45e8757cd1700821766a6a397c6fe
lianxmfor <1034552569@qq.com>
2022-04-23 20:32:12 +0800

    feat(store/lsm/memtable): implement get_index and get methods

--- src/store/lsm/memtable.cpp
+++ src/store/lsm/memtable.cpp
@@ -38,3 +38,40 @@ MemTable::~MemTable()
     entries.clear();
 }
 
+unsigned int MemTable::len()
+{
+    return usize;
+}
+
+bool MemTable::is_empty()
+{
+    return usize == 0;
+}
+
+auto MemTable::get_index(const string& key) -> tuple<decltype(entries)::iterator, bool>
+{
+    auto iter = std::lower_bound(entries.begin(), entries.end(), key,
+        [](const MemTableEntry& entry, const string& key) {
+            return entry.key < key;
+        });
+
+    return std::make_tuple(iter, iter != entries.end() && iter->key == key);
+}
+
+
+unique_ptr<MemTableEntry> MemTable::get(const string& key)
+{
+    decltype(entries)::iterator iter;
+    bool exist;
+
+    std::tie(iter, exist) = this->get_index(key);
+    if (!exist) {
+        return nullptr;
+    }
+
+    return unique_ptr<MemTableEntry>(new MemTableEntry(
+        iter->key,
+        iter->value,
+        iter->deleted,
+        iter->timestamp));
+}
