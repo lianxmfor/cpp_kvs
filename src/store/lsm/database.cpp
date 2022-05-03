0bdf3cec6057295d5e43318e353615bb58603df3
lianxmfor <lianxm2@gmail.com>
2022-05-03 12:24:01 +0800

    chore: foramt code

--- src/store/lsm/database.cpp
+++ src/store/lsm/database.cpp
@@ -17,6 +17,7 @@ using std::uint64_t;
 using std::unique_ptr;
 
 namespace fs = std::filesystem;
+namespace chrono = std::chrono;
 
 Database::Database(fs::path dir)
 {
@@ -24,7 +25,7 @@ Database::Database(fs::path dir)
 
     this->dir = dir;
     this->wal = std::move(wal);
-    this->memtable = memtable;
+    this->memtable = std::move(memtable);
 }
 
 fs::path Database::get_dir() const
@@ -32,20 +33,20 @@ fs::path Database::get_dir() const
     return dir;
 }
 
-auto Database::get(const string& key) -> tuple<std::optional<std::string>, unique_ptr<error>>
+auto Database::get(const string& key) -> tuple<optional<string>, unique_ptr<error>>
 {
     auto entry = memtable.get(key);
 
-    if (entry == nullptr || entry->value == std::nullopt) {
-        return std::make_tuple(nullopt, nullptr);
+    if (entry == nullptr || entry->value == nullopt) {
+        return make_tuple(nullopt, nullptr);
     }
 
-    return std::make_tuple(*entry->value, nullptr);
+    return make_tuple(*entry->value, nullptr);
 }
 
-auto Database::set(const std::string& key, const std::string& value) -> std::unique_ptr<error>
+auto Database::set(const string& key, const string& value) -> unique_ptr<error>
 {
-    uint64_t timestamp = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now())
+    uint64_t timestamp = chrono::time_point_cast<chrono::microseconds>(chrono::system_clock::now())
                              .time_since_epoch()
                              .count();
 
@@ -57,9 +58,9 @@ auto Database::set(const std::string& key, const std::string& value) -> std::uni
     return nullptr;
 }
 
-auto Database::remove(const std::string& key) -> std::unique_ptr<error>
+auto Database::remove(const string& key) -> unique_ptr<error>
 {
-    uint64_t timestamp = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now())
+    uint64_t timestamp = chrono::time_point_cast<chrono::microseconds>(chrono::system_clock::now())
                              .time_since_epoch()
                              .count();
 
